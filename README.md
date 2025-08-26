70ac6c4d6698700213e23bdd441ed28b3b89220b
woheller69 <woheller69@t-online.de>
2025-08-26 13:55:10 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,47 @@
+# FreeDroidWarn
+
+### Overview
+
+This library shows a deprecation warning because Google will require developer verification for Android apps outside the Play Store from 2026/2027 which the developer is not going to provide.
+
+https://www.androidauthority.com/android-developer-verification-requirements-3590911/
+
+https://developer.android.com/developer-verification
+
+
+### Installation
+
+Add the JitPack repository to your root build.gradle at the end of repositories:
+
+```gradle
+allprojects {
+  repositories {
+    ...
+    maven { url 'https://jitpack.io' }
+  }
+}
+```
+
+Add the library dependency to your build.gradle file.
+
+```gradle
+dependencies {
+    implementation 'com.github.woheller69:freeDroidWarn:V1.0'
+}
+```
+
+### Usage
+
+In onCreate of your app just add:
+
+```
+     FreeDroidWarn.showWarningOnUpgrade(this, BuildConfig.VERSION_CODE);
+
+```
+
+
+### License
+
+This library is licensed under the GPLv3.
+
+
---
+++ library/src/main/java/org/woheller69/freeDroidWarn/FreeDroidWarn.java
@@ -0,0 +1,53 @@
+package org.woheller69.freeDroidWarn;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+
+public class FreeDroidWarn {
+
+    static boolean shouldShowWarningDialog(Context context, int buildVersion) {
+        SharedPreferences prefManager = PreferenceManager.getDefaultSharedPreferences(context);
+        int versionCode = prefManager.getInt("versionCode",0);
+
+        if (prefManager.contains("versionCode") && buildVersion>versionCode){
+            SharedPreferences.Editor editor = prefManager.edit();
+            editor.putInt("versionCode", buildVersion);
+            editor.apply();
+            return true;
+        } else {
+            SharedPreferences.Editor editor = prefManager.edit();
+            editor.putInt("versionCode", buildVersion);
+            editor.apply();
+            return false;
+        }
+    }
+
+    static void showWarningOnUpgrade(Context context, int buildVersion){
+        if (shouldShowWarningDialog(context,buildVersion)){
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+            alertDialogBuilder.setMessage(R.string.dialog_Warning).setIcon(R.drawable.ic_warning);
+            alertDialogBuilder.setPositiveButton(context.getString(R.string.dialog_more_info), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://developer.android.com/developer-verification")));
+                }
+            });
+            alertDialogBuilder.setNegativeButton(context.getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
+
+    }
+
+}
