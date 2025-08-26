972612034d6d5e8cdb466c3d36d6798068801d25
woheller69 <woheller69@t-online.de>
2025-08-26 14:21:03 +0200

    Bugfix V1.1

--- library/src/main/java/org/woheller69/freeDroidWarn/FreeDroidWarn.java
+++ library/src/main/java/org/woheller69/freeDroidWarn/FreeDroidWarn.java
@@ -2,7 +2,6 @@ package org.woheller69.freeDroidWarn;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 
 import android.content.SharedPreferences;
@@ -13,16 +12,16 @@ public class FreeDroidWarn {
 
     static boolean shouldShowWarningDialog(Context context, int buildVersion) {
         SharedPreferences prefManager = PreferenceManager.getDefaultSharedPreferences(context);
-        int versionCode = prefManager.getInt("versionCode",0);
+        int versionCode = prefManager.getInt("versionCodeWarn",0);
 
-        if (prefManager.contains("versionCode") && buildVersion>versionCode){
+        if (prefManager.contains("versionCodeWarn") && buildVersion>versionCode){
             SharedPreferences.Editor editor = prefManager.edit();
-            editor.putInt("versionCode", buildVersion);
+            editor.putInt("versionCodeWarn", buildVersion);
             editor.apply();
             return true;
         } else {
             SharedPreferences.Editor editor = prefManager.edit();
-            editor.putInt("versionCode", buildVersion);
+            editor.putInt("versionCodeWarn", buildVersion);
             editor.apply();
             return false;
         }
@@ -31,17 +30,9 @@ public class FreeDroidWarn {
     public static void showWarningOnUpgrade(Context context, int buildVersion){
         if (shouldShowWarningDialog(context,buildVersion)){
             AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
-            alertDialogBuilder.setMessage(R.string.dialog_Warning).setIcon(R.drawable.ic_warning);
-            alertDialogBuilder.setPositiveButton(context.getString(R.string.dialog_more_info), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://developer.android.com/developer-verification")));
-                }
-            });
-            alertDialogBuilder.setNegativeButton(context.getString(android.R.string.ok), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                }
+            alertDialogBuilder.setTitle("").setMessage(R.string.dialog_Warning).setIcon(R.drawable.ic_warning);
+            alertDialogBuilder.setPositiveButton(context.getString(R.string.dialog_more_info), (dialog, which) -> context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://developer.android.com/developer-verification"))));
+            alertDialogBuilder.setNegativeButton(context.getString(android.R.string.ok), (dialog, which) -> {
             });
 
             AlertDialog alertDialog = alertDialogBuilder.create();
