97125aa1c5acc23454a941b4b848c9f8b1aab641
woheller69 <woheller69@t-online.de>
2025-09-24 12:07:46 +0200

    add solution button with link to info about uncensored OS versions or how to use ADB

--- library/src/main/java/org/woheller69/freeDroidWarn/FreeDroidWarn.java
+++ library/src/main/java/org/woheller69/freeDroidWarn/FreeDroidWarn.java
@@ -2,11 +2,15 @@ package org.woheller69.freeDroidWarn;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.preference.PreferenceManager;
+import android.widget.Button;
+
+import androidx.core.content.ContextCompat;
 
 public class FreeDroidWarn {
 
@@ -16,15 +20,20 @@ public class FreeDroidWarn {
         if (buildVersion > versionCode){
             AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
             alertDialogBuilder.setMessage(R.string.dialog_Warning);
-            alertDialogBuilder.setPositiveButton(context.getString(R.string.dialog_more_info), (dialog, which) -> context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://developer.android.com/developer-verification"))));
-            alertDialogBuilder.setNegativeButton(context.getString(android.R.string.ok), (dialog, which) -> {
+            alertDialogBuilder.setNegativeButton(context.getString(R.string.dialog_more_info), (dialog, which) -> context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://developer.android.com/developer-verification"))));
+            alertDialogBuilder.setPositiveButton(context.getString(android.R.string.ok), (dialog, which) -> {
                 SharedPreferences.Editor editor = prefManager.edit();
                 editor.putInt("versionCodeWarn", buildVersion);
                 editor.apply();
             });
+            alertDialogBuilder.setNeutralButton(context.getString(R.string.solution), (dialog, which) -> context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/woheller69/FreeDroidWarn?tab=readme-ov-file#workaround"))));
 
             AlertDialog alertDialog = alertDialogBuilder.create();
             alertDialog.show();
+            Button neutralButton = alertDialog.getButton(DialogInterface.BUTTON_NEUTRAL);
+            if (neutralButton != null) {
+                neutralButton.setTextColor(ContextCompat.getColor(context, android.R.color.holo_red_dark));
+            }
         }
 
     }
