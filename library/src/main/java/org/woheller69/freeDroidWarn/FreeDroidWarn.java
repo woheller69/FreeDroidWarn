d5cfd03b7ceec1b3c66b8afc8fc4b4e38c8668b5
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-02-25 18:20:51 +0100

    Use MaterialAlertDialogBuilder instead

--- library/src/main/java/org/woheller69/freeDroidWarn/FreeDroidWarn.java
+++ library/src/main/java/org/woheller69/freeDroidWarn/FreeDroidWarn.java
@@ -1,6 +1,5 @@
 package org.woheller69.freeDroidWarn;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -8,9 +7,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.preference.PreferenceManager;
+import android.util.TypedValue;
 import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class FreeDroidWarn {
 
@@ -18,21 +21,27 @@ public class FreeDroidWarn {
         SharedPreferences prefManager = PreferenceManager.getDefaultSharedPreferences(context);
         int versionCode = prefManager.getInt("versionCodeWarn",0);
         if (buildVersion > versionCode){
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
-            alertDialogBuilder.setMessage(R.string.dialog_Warning);
-            alertDialogBuilder.setNegativeButton(context.getString(R.string.dialog_more_info), (dialog, which) -> context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://keepandroidopen.org"))));
-            alertDialogBuilder.setPositiveButton(context.getString(android.R.string.ok), (dialog, which) -> {
+            MaterialAlertDialogBuilder materialAlertDialogBuilder = new MaterialAlertDialogBuilder(context);
+            materialAlertDialogBuilder.setMessage(R.string.dialog_Warning);
+            materialAlertDialogBuilder.setNegativeButton(context.getString(R.string.dialog_more_info), (dialog, which) -> context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://keepandroidopen.org"))));
+            materialAlertDialogBuilder.setPositiveButton(context.getString(android.R.string.ok), (dialog, which) -> {
                 SharedPreferences.Editor editor = prefManager.edit();
                 editor.putInt("versionCodeWarn", buildVersion);
                 editor.apply();
             });
-            alertDialogBuilder.setNeutralButton(context.getString(R.string.solution), (dialog, which) -> context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/woheller69/FreeDroidWarn?tab=readme-ov-file#solutions"))));
+            materialAlertDialogBuilder.setNeutralButton(context.getString(R.string.solution), (dialog, which) -> context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/woheller69/FreeDroidWarn?tab=readme-ov-file#solutions"))));
 
-            AlertDialog alertDialog = alertDialogBuilder.create();
+            AlertDialog alertDialog = materialAlertDialogBuilder.create();
             alertDialog.show();
             Button neutralButton = alertDialog.getButton(DialogInterface.BUTTON_NEUTRAL);
             if (neutralButton != null) {
-                neutralButton.setTextColor(ContextCompat.getColor(context, android.R.color.holo_red_dark));
+                TypedValue tv = new TypedValue();
+                context.getTheme().resolveAttribute(com.google.android.material.R.attr.colorErrorContainer, tv, true);
+                int color = tv.resourceId != 0
+                        ? ContextCompat.getColor(context, tv.resourceId)
+                        : tv.data;
+
+                neutralButton.setTextColor(color);
             }
         }
 
